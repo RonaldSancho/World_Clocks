5fa4b3310b7f7e6eec07a53dbdd5b85036a4efe6
Ronald Sancho <ronalsancho@gmail.com>
2023-11-19 01:11:31 -0600

    Change API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import DateTime from "./DateTime";
 
 function App() {
-    return <h1>Hello</h1>
+    return (
+        <div>
+            <h1>Hello</h1>
+            <DateTime/>
+        </div>
+    )
 }
 
 export default App;
---
+++ src/components/DateTime.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import DataTime from "../DataTime";
+
+function DateTime(){
+
+    const [dateTime, setDateTime] = useState(null);
+
+    useEffect(() => {
+        async function fetchData(){
+            try{
+                const result = await DataTime();
+                setDateTime(result);
+            } catch (error){
+                console.error('Error')
+            }
+        }
+        fetchData();
+    }, []);
+    
+    if(!dateTime){
+        return <div>Error</div>;
+    }
+
+    return (
+        <div>
+            <p>Fecha: {dateTime[0].date}</p>
+            <p>Fecha: {dateTime[1].time}</p>
+        </div>
+    );
+}
+
+export default DateTime;
