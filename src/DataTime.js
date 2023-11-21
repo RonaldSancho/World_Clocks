ff09f3ead315210f10064f037d10155e826f0120
Ronald Sancho <ronalsancho@gmail.com>
2023-11-20 22:49:10 -0600

    Modify DataTime.js and add map function

--- src/DataTime.js
+++ src/DataTime.js
@@ -1,21 +1,32 @@
 import axios from "axios";
+import { response } from "express";
 
-const apiURl = "http://worldtimeapi.org/api/timezone/America/Costa_Rica";
+const locations = [
+    "America/Costa_Rica",
+    "America/New_York",
+    "Asia/Shangai",
+    "Asia/Kolkata",
+    "Europe/Berlin",
+    "Asia/Tokyo"
+
+
+]
 
 async function DataTime(){
     try{
-        const reponse = await axios.get(apiURl);
-        const dataTime = reponse.data;
-    
-        const date = dataTime.datetime.split('T')[0];
-        const time = dataTime.datetime.split('T')[1].slice(0,8);
-        const zone = dataTime.timezone;
+        const responses = await Promise.all(locations.map(async (location) => {
+            const apiURL = `http://worldtimeapi.org/api/timezone/${location}`;
+            const reponse = await axios.get(apiURL);
+            return reponse.data;
+        }));
 
-        const arrayDateTime = [
-            {date: date,
-            time: time,
-            timezone: zone}
-        ];
+        const arrayDateTime = responses.map((dataTime, index) => ({
+            country: locations[index].split('/')[1],
+            date: dataTime.datetime.split('T')[0],
+            time: dataTime.datetime.split('T')[1].slice(0,8),
+            zone: dataTime.timezone,
+        }))
+
         return arrayDateTime;
     } catch (error){
         console.error('Error al obtener los datos')
