ea03038a872be0015f0ffd54d780203fd78787dd
Ronald Sancho <ronalsancho@gmail.com>
2023-11-30 18:13:00 -0600

    Fix footer styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ function Footer(){
     const date = new Date().getFullYear();
 
     return  (
-            <footer>
-                <p>Copyright {date}</p>
-            </footer>
+        <footer>
+            <p>&copy; 2023 World Clock &bull; All rights reserved &bull; Made By <a href="https://ronaldsancho.github.io/Portfolio_Ronald/">Ronald Sancho</a></p>
+        </footer>
     );
 }
 
